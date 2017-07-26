cd2e6e0cea1961a46adfa19b5c57123791186820
WaylanPunch <waylanpunch@gmail.com>
2017-07-27 01:12:20 +0800

    Signed-off-by: WaylanPunch <waylanpunch@gmail.com>

---
+++ src/cn/zju/action/IndexAction.java
@@ -0,0 +1,13 @@
+package cn.zju.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class IndexAction extends ActionSupport {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return ActionSupport.SUCCESS;
+	}
+
+}
